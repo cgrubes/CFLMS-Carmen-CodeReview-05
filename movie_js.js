3ee61e77381b869d7f7323e3f6eb30eb9c37f3e5
carmen <grubes@gmail.com>
2020-06-12 14:28:17 +0200

    made movie website with like button

---
+++ movie_js.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+    var movie_array = JSON.parse(movies);
+
+    for (i = 0; i < movie_array.length; i++) {
+
+        let movie = `<div id="${i}" class = "movie">
+        <img src="${movie_array[i].image}">
+            
+            <div class="info">
+            <div class = "name">
+             <p><h3>${movie_array[i].name}</h3></p>
+             </div>
+             
+             <div class = "desc">
+             <br>
+             <p>${movie_array[i].description}</p>
+             </div>
+
+             <div class = "button">
+             <button>Like &#128077;</button>
+             <div class="likes">  </div>
+             </div>
+
+         </div>`;
+
+
+        $(".wrapper").append(movie);
+    }
+
+    var likes = 0;
+        $(".button").on("click", function(){
+        likes += 1;
+            $(".likes").text(likes);
+ 
+        })
+
+
+})
